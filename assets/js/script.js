57ea1acf766ebcfac39572150fd0360d69096f6d
camilarjs <105024537+camilarjs@users.noreply.github.com>
2022-05-14 00:39:58 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $("a").click(function() {
+    var gato = this.hash
+    $("html, body").animate(
+  
+      {
+        scrolltop: $(gato).offset().top
+      },
+      3000
+  
+    )
+  
+  })
+  })
+  
+  
+  
+  
+  
+  
+  //tooltip//
+  
+  var tooltipTriggerList =
+  [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+  
